7202efcdb998c018cb5bca33dcfdd0cc9f789e81
swsw <swsw1005@gmail.com>
2020-04-23 23:51:03 +0900

    aaa

---
+++ WebContent/ch02_data_io/write.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>글쓰기</h1>
+글번호: ${param.no }<br>
+제목: ${param.title }<br>
+<a href="attrFormData.html">입력 페이지 이동</a>
+</body>
+</html>
---
+++ WebContent/swsw/Test.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.HttpJspPage;
+import javax.servlet.jsp.JspPage;
+
+public class Test extends HttpServlet implements HttpJspPage {
+
+    public void service(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
